e4b04240629b82d7c347bb167a0cd6aee0558084
gallasglasses <gallasglasses@gmail.com>
2021-05-19 11:56:28 +0300

    re-loops merged into printArray function, function renaming, comments added

--- HW5/HW5/HW5.cpp
+++ HW5/HW5/HW5.cpp
@@ -3,88 +3,90 @@
 #include <iostream>
 #include <cmath>
 
-void arrayHW1(double* array, int size);
-int HW2(int* array, int i);
-void HW3(int* array, int size);
-void HW4(int x, int* array, int size);
+void printArray(double* array, size_t size);
+void printArray(int* array, size_t size);
+void inverseBinArray(int* array, size_t size);
+void fillArray(int* array, size_t size);
+void shiftArray(int x, int* array, size_t size);
 int shiftDigit(int a, int b);
-bool HW5(int* array, int size);
+bool HW5(int* array, size_t size);
 
 int main()
 {
-    std::cout << "This is 1 exercise\n\n";
+    std::cout << "This is 1 exercise\n"
+        "Write a function that prints an array of double numbers to the screen.\n\n";
+
     {
-        const int SIZE = 10;
+        const size_t SIZE = 10;
         double array[SIZE] = { 1.2,1.9,2.3,4.5,6.2,1.7,2.9,0.1,3.3,9.9 };
-        arrayHW1(array, SIZE);
+        printArray(array, SIZE);
+        std::cout << "\n_____________________________________________\n\n";
     }
 
-    std::cout << "This is 2 exercise\n\n";
+    std::cout << "This is 2 exercise\n"
+        "Specify an integer array of elements 0 and 1.\n"
+        "Write a function that replaces 0 by 1, 1 by 0 in the received array.\n\n";
+
     {
-        const int SIZE = 10;
+        const size_t SIZE = 10;
         int array[SIZE] = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
 
         std::cout.width(25);
         std::cout << std::right << "Array before changes: ";
-        for (int i = 0; i < SIZE; i++)
-        {
-            std::cout << array[i] << ' ';
-        }
+        printArray(array, SIZE);
         std::cout << "\n";
         std::cout.width(25);
         std::cout << std::right << "Array after changes: ";
-        for (int i = 0; i < SIZE; i++)
-        {
-            std::cout << HW2(array, i) << ' ';
-        }
+        inverseBinArray(array, SIZE);
+        printArray(array, SIZE);
         std::cout << "\n_____________________________________________\n\n";
     }
 
-    std::cout << "This is 3 exercise\n\n";
+    std::cout << "This is 3 exercise\n"
+        "Set an empty integer array of size 8.\n"
+        "Write a function that will use a loop to fill it with values 1 4 7 10 13 16 19 22.\n\n";
 
     {
-        const int SIZE = 8;
+        const size_t SIZE = 8;
         int array[SIZE] = { };
 
-        HW3(array, SIZE);
-
-        for (int i : array)
-        {
-            std::cout << i << ' ';
-        }
+        fillArray(array, SIZE);
+        printArray(array, SIZE);
         std::cout << "\n_____________________________________________\n\n";
     }
 
-    std::cout << "This is 4 exercise\n\n";
+    std::cout << "This is 4 exercise\n"
+        "Write a function that accepts a one-dimensional array \n"
+        "and a number n (can be positive or negative) as input, \n"
+        "and the method must cyclically shift all elements of \n"
+        "the array by n positions.\n\n";
 
     {
-        const int SIZE = 10;
+        const size_t SIZE = 10;
         int array[SIZE] = {0,1,2,3,4,5,6,7,8,9};
         int n;
         std::cout << "Enter the number to shift " 
             "(positive number - shift to the right, negative number - shift to the left) ";
         std::cin >> n;
         
-        HW4(n, array, SIZE);
-        for (int i : array)
-        {
-            std::cout << i << ' ';
-        }
+        shiftArray(n, array, SIZE);
+        printArray(array, SIZE);
         std::cout << "\n_____________________________________________\n\n";
     }
 
-    std::cout << "This is 5 exercise\n\n";
+    std::cout << "This is 5 exercise\n"
+        "Write a function that is passed a non-empty one-dimensional \n"
+        "integer array, the function must return true if there is \n"
+        "a place in the array where the sum of the left and right \n"
+        "sides of the array are equal.\n\n";
 
     {
-        const int SIZE = 5;
+        const size_t SIZE = 5;
         int array[SIZE] = { 1,2,3,4,10 };
         
-        
-        for (int i : array)
-        {
-            std::cout << i << ' ';
-        }
-        std::cout << "\n";
+        std::cout << "Checking the balance of this array: ";
+        printArray(array, SIZE);
+        std::cout << "\n\n";
 
         if (HW5(array, SIZE))
             std::cout << " - true\n";
@@ -97,40 +99,55 @@ int main()
     return 0;
 }
 
-void arrayHW1(double * array, int size)
+void printArray(double * array, size_t size)
 {
-    for (int i = 0; i < size ; i++)
+    for (size_t i = 0; i < size ; i++)
     {
         std::cout << array[i] << ' ';
     }
-    std::cout << "\n_____________________________________________\n\n";
+    
 }
 
-int HW2(int* array, int i)
+void printArray(int* array, size_t size)
 {
-    return ((array[i] == 1) ? array[i] = 0 : array[i] = 1);
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << array[i] << ' ';
+    }
+
 }
 
-void HW3(int* array, int size)
+void inverseBinArray(int* array, size_t size)
 {
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
+    {
+        array[i] = (array[i] == 1) ? 0 : 1;
+    }
+}
+
+void fillArray(int* array, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
     {
         array[i] = 1 + i * 3;
     }
 }
 
-void HW4(int x, int* array, int size)
+void shiftArray(int x, int* array, size_t size)
 {
     int shift,
         remember = 0,
         currentIndex = 0,
         shiftedIndex;
-      
+    if (x == 0)
+    {
+        return;
+    }
     if (abs(x) > size)
         shift = x % size;
     else shift = x;
     //std::cout << "shift: " << shift << "\n";
-    for (int i = size-1; i >= (size - shiftDigit(abs(shift), size)); i--)
+    for (size_t i = size-1; i >= (size - shiftDigit(abs(shift), size)); i--)
     {
         remember = array[i];
         currentIndex = i;
@@ -171,6 +188,7 @@ void HW4(int x, int* array, int size)
     
 }
 
+//Find G.C.D using recursion
 int shiftDigit(int a, int b)
 {
     if (b == 0)
@@ -179,17 +197,17 @@ int shiftDigit(int a, int b)
         return shiftDigit(b, a % b);
 }
 
-bool HW5(int* array, int size)
+bool HW5(int* array, size_t size)
 {
     int sum = 0;
     bool isBalanceArray = false;
-    for (int i = 0; i< size; i++)
+    for (size_t i = 0; i< size; i++)
     {
         sum += array[i];
     }
     int checkLeftPart = 0,
         leftBorder = 0;
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
     {
         checkLeftPart += array[i];
         if (checkLeftPart == sum - checkLeftPart)
